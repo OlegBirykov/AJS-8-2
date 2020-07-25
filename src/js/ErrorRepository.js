084c80ad9a51672d0d9110cd6f0876e947445552
OlegBirykov <p_petro75@mail.ru>
2020-07-25 21:18:23 +0300

    Добавлен код и тесты

--- src/js/ErrorRepository.js
+++ src/js/ErrorRepository.js
@@ -1,3 +1,14 @@
 export default class ErrorRepository {
+  constructor() {
+    this.members = new Map([
+      [0, 'Fatal error'],
+      [1, 'No connect'],
+      [2, 'Argument not found'],
+      [3, 'Not enough memory'],
+    ]);
+  }
 
+  translate(code) {
+    return this.members.has(code) ? this.members.get(code) : 'Unknown error';
+  }
 }
--- src/js/__tests__/ErrorRepository.test.js
+++ src/js/__tests__/ErrorRepository.test.js
@@ -1,5 +1,18 @@
 import ErrorRepository from '../ErrorRepository';
 
-test('', () => {
-  expect(new ErrorRepository()).toDefined();
+const err = new ErrorRepository();
+
+test.each([
+  [0, 'Fatal error'],
+  [1, 'No connect'],
+  [2, 'Argument not found'],
+  [3, 'Not enough memory'],
+])(
+  'Должен выводиться текст при известном коде ошибки', (code, text) => {
+    expect(err.translate(code)).toBe(text);
+  },
+);
+
+test('Должен выводиться текст при неизвестном коде ошибки', () => {
+  expect(err.translate(-1)).toBe('Unknown error');
 });
